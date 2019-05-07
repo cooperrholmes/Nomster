fdb815ffc0636ffce08023b34a6dc59b56a0f292
cooperrholmes@gmail.com <cooperrholmes@gmail.com>
2019-05-07 00:53:50 +0000

    Added email functionality

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,9 @@
+class NotificationMailer < ApplicationMailer
+  default from: "no-replay@nomsterapp.com"
+
+  def comment_added
+    mail(to: "cooperrholmes@gmail.com",
+         subject: "A comment has been added to your place")
+  end
+
+end
